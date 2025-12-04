cdf38a29cdc18ab21fc7ed1676d289a0897f380f
NG Estratégia Digital <161879316+NathanSaraiva11TIA@users.noreply.github.com>
2025-12-04 03:48:20 -0300

    é

--- ranking/ranking.js
+++ ranking/ranking.js
@@ -18,7 +18,7 @@ document.addEventListener("click", (e) => {
 // Função para carregar ranking
 async function carregarRanking() {
   try {
-    const resposta = await fetch("http://localhost:3333/ranking");
+    const resposta = await fetch("https://backend-production-ec72d.up.railway.app/ranking");
 
     if (!resposta.ok) {
       throw new Error(`Erro HTTP: ${resposta.status}`);
