7c3258fb27adab3c326da211b916f49a5012f4a3
NG Estratégia Digital <161879316+NathanSaraiva11TIA@users.noreply.github.com>
2025-12-05 00:20:39 -0300

    EU NAO AGUENTO MAIS pt-3

--- login/login.js
+++ login/login.js
@@ -8,7 +8,7 @@ async function logar() {
   }
 
   try {
-    const resposta = await fetch("https://backend-production-ec72d.up.railway.app/login", {
+    const resposta = await fetch("http://localhost:3333/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, senha })
